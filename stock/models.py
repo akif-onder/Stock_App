f6ed5b81cba650f30cf49e842ec411474c018d2b
akif-onder <akifonder@gmail.com>
2022-10-06 22:08:24 +0300

    view

--- stock/models.py
+++ stock/models.py
@@ -1,11 +1,7 @@
-
 from random import choices
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
-# Create your models here.
 
 class UpdateCreate(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -13,10 +9,8 @@ class UpdateCreate(models.Model):
 
     class Meta:
         abstract = True
-    
-    def __str__(self):
-        return self.name
-    
+
+
 class Category(models.Model):
     name = models.CharField(max_length=25)
 
@@ -25,18 +19,23 @@ class Category(models.Model):
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=50)
-    
+    name = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
 
+
 class Product(UpdateCreate):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE,related_name='b_products' )
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='products')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='b_products')
     stock = models.SmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Firm(UpdateCreate):
     name = models.CharField(max_length=30)
@@ -45,19 +44,23 @@ class Firm(UpdateCreate):
 
     def __str__(self):
         return self.name
-    
+
+
 class Transaction(UpdateCreate):
     TRANSACTION = (
-        ('I','IN'),
-        ('O', 'OUT')
+        (1, 'IN'),
+        (0, 'OUT')
     )
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    firm = models.ForeignKey(Firm, on_delete=models.SET_NULL, null=True, related_name='transactions')
+    firm = models.ForeignKey(
+        Firm, on_delete=models.SET_NULL, null=True, related_name='transactions')
     transaction = models.SmallIntegerField(choices=TRANSACTION)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='transactions' )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='transaction')
     quantity = models.SmallIntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    price_total = models.DecimalField(max_digits=9, decimal_places=2)
+    price_total = models.DecimalField(
+        max_digits=8, decimal_places=2, blank=True)
 
     def __str__(self):
         return f'{self.transaction} - {self.product} - {self.quantity}'
